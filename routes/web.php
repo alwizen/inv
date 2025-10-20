810dc97bed3228f88c85a2a8216a90c184d9fac9
alwizen <solu8i.project@gmail.com>
2025-10-20 16:29:24 +0700

    Refactor route definitions in web.php: simplify welcome route and add redirect for login

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,9 @@
 use App\Http\Controllers\InvoicePdfController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => view('welcome'));
 
-// Route::get('/invoice/{invoice}/pdf', [\App\Http\Controllers\InvoicePdfController::class, 'generate'])->name('invoice.pdf');
+Route::redirect('/login', '/admin/login', 302); // <-- tambah ini
 
-
-Route::get('/invoice/{invoice}/pdf',  [InvoicePdfController::class, 'generate'])->name('invoice.pdf');
-Route::get('/invoice/{invoice}/pdf2', [InvoicePdfController::class, 'generateV2'])->name('invoice.pdf2');
+Route::get('/invoice/{invoice}/pdf',  [\App\Http\Controllers\InvoicePdfController::class, 'generate'])->name('invoice.pdf');
+Route::get('/invoice/{invoice}/pdf2', [\App\Http\Controllers\InvoicePdfController::class, 'generateV2'])->name('invoice.pdf2');
